b0b896abadf219583f33976e6ffc7c89c578c1a4
Mihaly Balogh <tigerinformation00@gmail.com>
2019-05-31 09:11:25 +0200

    still starting

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -11,6 +11,14 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PagesController@index');
+Route::get('/about', 'PagesController@about');
+Route::get('/services', 'PagesController@services');
+
+// Route::get('/about', function(){
+//     return view('pages.about');
+// });
+
+// Route::get('/users/{id}', function($id){
+//     return 'This is user '.$id;
+// });
