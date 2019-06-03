03b928f1a8b80af5f7fde928d0cc6ca052fbfe3b
Mihaly Balogh <tigerinformation00@gmail.com>
2019-06-03 17:07:23 +0200

    changes

--- laravel/resources/views/inc/navbar.blade.php
+++ laravel/resources/views/inc/navbar.blade.php
@@ -15,7 +15,15 @@
 			<li class="nav-item">
 				<a class="nav-link" href="/services">Services</a>
 			</li>
-		  </ul>
+			<li class="nav-item">
+					<a class="nav-link" href="/posts">Blog</a>
+			</li>
+			</ul>
+			<ul class="nav navbar-nav navbar-right">
+					<li class="nav-item">
+							<a class="nav-link" href="/posts/create">Create Post</a>
+					</li>
+			</ul>
 		</div>
 </nav>
 <br>
